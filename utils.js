f1c7d5ef4b5afc8530099cdeda94a0ed96464cf4
Daniele Sabre <daniele.sabre@jakala.com>
2022-05-26 13:33:12 +0200

    Develop

--- utils.js
+++ utils.js
@@ -3,11 +3,6 @@ const env   = process.env.NODE_ENV || 'development';
 const isDev = env === 'development';
 
 const loadConfig = () => {
-	// function enabled only in development
-	if (!isDev) {
-		return;
-	}
-	
 	// lock if already loaded
 	if (!!process.env.CONFIG_LOADED) {
 		return;
@@ -22,22 +17,6 @@ const loadConfig = () => {
 		console.log(chalk.red(`No ${envFile} file configuration found.`));
 		process.exit(1);
 	}
-	
-	// load config from heroku
-	// const {execSync}       = require("child_process");
-	// let herokuConfig       = execSync('heroku config -j');
-	// herokuConfig           = Buffer.from(herokuConfig).toString();
-	// herokuConfig           = JSON.parse(herokuConfig);
-	// const herokuConfigKeys = Object.keys(herokuConfig);
-	// for (let i = 0; i < herokuConfigKeys.length; i++) {
-	//     let key = herokuConfigKeys[i];
-	//
-	//     if (!!process.env[key]) {
-	//         continue;
-	//     }
-	//
-	//     process.env[key] = herokuConfig[key].trim();
-	// }
 	
 	// set env to avoid multiple config loads
 	process.env.CONFIG_LOADED = '1';
